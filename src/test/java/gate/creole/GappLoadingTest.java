2ea20748cbe2ba733d69cfe743db371f1a9b77fd
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2018-05-14 18:58:10 +0100

    first drop of code

---
+++ src/test/java/gate/creole/GappLoadingTest.java
@@ -0,0 +1,10 @@
+package gate.creole;
+
+import gate.test.GappLoadingTestCase;
+
+public class GappLoadingTest extends GappLoadingTestCase {
+
+  public GappLoadingTest() {
+    super("pipeline.gapp","sample_linked_data_mashup.gapp");
+  }
+}
